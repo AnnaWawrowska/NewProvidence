4bb8ed680e596730024dbad4226ab4ffc07f2846
Anna Wawrowska <anna.wawrowska.it@gmail.com>
2017-11-15 21:17:29 +0100

    Fix images names, fix responsive styles

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -27,8 +27,27 @@ $(document).ready(function(){
 
   $('.button__down').click(function() {
     $('html, body').animate({
-      scrollTop: $(".section--clients").offset().top
+      scrollTop: $('.section--clients').offset().top
     },
     'slow');
   });
+
+
+  $('.nav__toggle').click(function() {
+    $(this).toggleClass('nav__toggle--active');
+    $('.nav__list').toggleClass('nav__list--visibile');
+  });
+});
+
+var change = $('.section--clients').offset()
+
+$(document).scroll(function() {
+  var scroll__start = $(this).scrollTop();
+  if(scroll__start > change.top - 59) {
+    $('.header').addClass('header--background');
+    $('.nav__list').addClass('nav__list--background')
+  } else {
+    $('.header').removeClass('header--background');
+    $('.nav__list').removeClass('nav__list--background')
+  }
 });

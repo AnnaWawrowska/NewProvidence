703cbf6db110b9b7ce98b8b02e43a9a9b07c79c8
Anna Wawrowska <anna.wawrowska.it@gmail.com>
2017-12-28 00:58:19 +0100

    fix responsive styles and refactor scripts

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,53 +1,71 @@
-$(document).ready(function(){
-  $('.slider').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    prevArrow: $('.slider__arrow--prev'),
-    nextArrow: $('.slider__arrow--next'),
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-        }
-      }
-    ]
+(function () {
+  $(document).ready(function(){
+    project.sliderInit();
+    project.heroBackground();
+    project.scrollDown();
+    project.mobileNav();
   });
 
-  var canvasParent = $('.section--hero')
-  var pattern = Trianglify({
-    height: canvasParent.height(),
-    width: canvasParent.width(),
-    cell_size: 80,
-    x_colors: 'Blues'
+  $(document).scroll(function() {
+    project.mobileHeader();
   });
-  $('.canvas-wrapper').append(pattern.canvas());
 
+  $( window ).resize(function() {
+    console.log("resize");
+    project.heroBackground();
+  });
 
-  $('.button__down').click(function() {
-    $('html, body').animate({
-      scrollTop: $('.section--clients').offset().top
+  var project = {
+    sliderInit: function() {
+      $('.slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: $('.slider__arrow--prev'),
+        nextArrow: $('.slider__arrow--next'),
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+            }
+          }
+        ]
+      });
     },
-    'slow');
-  });
-
-
-  $('.nav__toggle').click(function() {
-    $(this).toggleClass('nav__toggle--active');
-    $('.nav__list').toggleClass('nav__list--visibile');
-  });
-});
-
-var change = $('.section--clients').offset()
-
-$(document).scroll(function() {
-  var scroll__start = $(this).scrollTop();
-  if(scroll__start > change.top - 59) {
-    $('.header').addClass('header--background');
-    $('.nav__list').addClass('nav__list--background')
-  } else {
-    $('.header').removeClass('header--background');
-    $('.nav__list').removeClass('nav__list--background')
-  }
-});
+    heroBackground: function() {
+      var canvasParent = $('.section--hero')
+      var pattern = Trianglify({
+        height: canvasParent.height(),
+        width: canvasParent.width(),
+        cell_size: 80,
+        x_colors: 'Blues'
+      });
+      $('.canvas-wrapper').html(pattern.canvas());
+    },
+    scrollDown: function() {
+      $('.button__down').click(function() {
+        $('html, body').animate({
+          scrollTop: $('.section--clients').offset().top
+        },
+        'slow');
+      });
+    },
+    mobileNav: function() {
+      $('.nav__toggle').click(function() {
+        $(this).toggleClass('nav__toggle--active');
+        $('.header').toggleClass('header--nav-open');
+        $('.nav__list').toggleClass('nav__list--visibile');
+      });
+    },
+    clientsSectionOffset: $('.section--clients').offset(),
+    mobileHeader: function() {
+      var scroll__start = $(document).scrollTop();
+      if(scroll__start > this.clientsSectionOffset.top - 59) {
+        $('.header').addClass('header--scrolled');
+      } else {
+        $('.header').removeClass('header--scrolled');
+      }
+    }
+  };
+})();
